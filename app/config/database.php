46f3e8b73b2298846879406fbe085d3d6510c3db
Neozelkova <nettech1992@gmail.com>
2014-10-21 19:26:48 -0400

    Database change
    DB change

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'odot',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'forge',
+			'username'  => 'forge',
+			'password'  => 'zTsxENZuhrZLhnzzHe1x',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
